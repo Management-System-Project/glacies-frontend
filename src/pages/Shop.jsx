e95983e6be7ae69d0475d85a4335210d5c6545ac
bohdan <shovkopliasbohdan@gmail.com>
2024-01-26 13:49:55 +0200

    shop page(no adaptive yet)

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { Edit } from "./components/UtilityButtons";
+import { Edit, EditPhoto } from "./components/UtilityButtons";
 
 import "../scss/shop.scss";
 
 function Shop() {
    const [rating, setRating] = useState(4);
-   const [auth, setAuth] = useState(false);
+   const [auth, setAuth] = useState(true);
    return (
       <main className="main shop">
          <section className="hero">
@@ -18,10 +18,7 @@ function Shop() {
                </div>
                {auth && <Edit />}
             </div>
-            <div className="photo">
-               <img src="" alt="photo-placeholder" />
-               {auth && <Edit />}
-            </div>
+            <Photo auth={auth}/>
             <div className="menu">
                <NavLink>Home</NavLink>
                <NavLink>Contacts</NavLink>
@@ -40,10 +37,40 @@ function Shop() {
                blanditiis quibusdam autem minus praesentium sint! Sequi iusto
                adipisci ut provident, necessitatibus iste architecto. Et
                mollitia dolorum dolores inventore placeat, dicta iste!
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+               modi, eos non libero a esse. Minus fuga iusto necessitatibus. Est
+               a nostrum, corporis hic porro, provident pariatur ullam
+               blanditiis quibusdam autem minus praesentium sint! Sequi iusto
+               adipisci ut provident, necessitatibus iste architecto. Et
+               mollitia dolorum dolores inventore placeat, dicta iste!
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+               modi, eos non libero a esse. Minus fuga iusto necessitatibus. Est
+               a nostrum, corporis hic porro, provident pariatur ullam
+               blanditiis quibusdam autem minus praesentium sint! Sequi iusto
+               adipisci ut provident, necessitatibus iste architecto. Et
+               mollitia dolorum dolores inventore placeat, dicta iste!
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+               modi, eos non libero a esse. Minus fuga iusto necessitatibus. Est
+               a nostrum, corporis hic porro, provident pariatur ullam
+               blanditiis quibusdam autem minus praesentium sint! Sequi iusto
+               adipisci ut provident, necessitatibus iste architecto. Et
+               mollitia dolorum dolores inventore placeat, dicta iste!
             </p>
             {auth && <Edit />}
          </section>
       </main>
+   );
+}
+
+function Photo({auth}) {
+   const [photoSrc, setPhotoSrc] = useState(
+      "https://i.pinimg.com/564x/bf/3b/bb/bf3bbb71ceb2aeefc3e6091439e35212.jpg"
+   );
+   return (
+      <div className="photo">
+         {photoSrc && <img src={photoSrc} alt="photo-placeholder" />}
+         {auth && <EditPhoto />}
+      </div>
    );
 }
 
